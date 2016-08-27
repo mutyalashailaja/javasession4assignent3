6fd123cc74bfa81a45c67eebce0100943440baab
mutyalashailaja <mutyala.shailaja@gmail.com>
2016-08-27 08:08:31 +0530

    session4assignment3

---
+++ src/employeedetails.java
@@ -0,0 +1,32 @@
+
+public class employeedetails {
+	
+	private String empName;
+	private String empId;
+	private int empAge;
+	
+	public String getempName(){
+		return empName;
+	}
+public String getempId(){
+	return empId;
+	
+}
+public int getempAge(){
+	return empAge;
+}
+public void setempName(String newempName){
+	empName=newempName;
+	
+}
+public void setempId(String newempId){
+	empId=newempId;
+}
+public void setempAge(int newempAge){
+	empAge=newempAge;
+}
+
+
+
+
+}
---
+++ src/encaptest.java
@@ -0,0 +1,15 @@
+
+public class encaptest extends employeedetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		encaptest encap=new encaptest();
+		encap.setempName("shailaja");
+		encap.setempId("101");
+		encap.setempAge(20);
+		System.out.print("Name : " + encap.getempName() + " Age : " +
+		encap.getempAge() + "Id :" + encap.getempId());
+
+	}
+
+}
